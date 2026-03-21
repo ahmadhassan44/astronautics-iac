f76432782162747fc5ee67399dbf87bb87dd626e
Ahmad Hassan <ahmad.tanveer@ccript.com>
2026-03-21 20:24:35 +0500

    feat/init

---
+++ providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "fLseUTB9xbh9jSHyXyB/M0aiVeQs/vCDnXVDE+pr"
+}
