032e73fc6fdae5841a97889cb7f350621a4c7619
Ahmad Hassan <ahmadhassatanveer44@gamil.com>
2026-03-21 23:39:49 +0500

    feat/make the iac managed resource

--- main/main.tf
+++ main/main.tf
@@ -2,3 +2,13 @@ import {
   to = aws_key_pair.managed_key
   id = "astro-ec2s-kp"
 }
+
+
+resource "aws_key_pair" "managed_key" {
+  key_name   = "astro-ec2s-kp"
+  public_key = ""
+
+  lifecycle {
+    ignore_changes = [public_key]
+  }
+}
