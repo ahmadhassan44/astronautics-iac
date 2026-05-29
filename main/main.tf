687ae562a69ca2f80e4cf04d7cdfb510208039f0
Ahmad Hassan <ahmad.tanveer@ccript.com>
2026-05-29 21:17:11 +0500

    chore/remove old kp

--- main/main.tf
+++ main/main.tf
@@ -1,11 +1,2 @@
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
-
-resource "aws_key_pair" "managed_key" {
-  key_name   = "astro-ec2s-kp"
-  public_key = ""
-
-  lifecycle {
-    ignore_changes = [public_key]
-  }
-}
